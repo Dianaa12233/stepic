68ab732877d919d243cc4d2084193d93db5afa7d
Dianaa12233 <116952930+Dianaa12233@users.noreply.github.com>
2022-11-05 12:32:56 +0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo mv /home/box/StepikNginx/web ~
+sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
